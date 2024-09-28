d618568aa9c8445c9e5f256aaf238da4a25dcb0e
Nicholas Fisher <102126354+FishyStix12@users.noreply.github.com>
2024-09-28 04:10:20 -0400

    Create-after-bluetth-piconf.sh

---
+++ kaliconfigdots/after-bluetth-piconf.sh
@@ -0,0 +1,7 @@
+# Run this after connecting to keyboard via bluetooth
+echo "================================================================================="
+echo "Important Information:"
+echo -e "If this is the Raspberry Pi please utilize the next selection of screens ton\to format the Pi's 4 inch square display!n\Go to 1 Setup Displayn\S060 HyperPixeln\tbd\nIf not hit Ctrl+C"
+echo "================================================================================="
+read -p "Press Enter to continue..."
+kalipi-tft-config
