174f5f80be6a831036fdf242cea2f50e3df3b076
Nicholas Fisher <102126354+FishyStix12@users.noreply.github.com>
2024-09-28 05:09:05 -0400

    Update dotkaliconf.sh

--- kaliconfigdots/dotkaliconf.sh
+++ kaliconfigdots/dotkaliconf.sh
@@ -46,7 +46,7 @@ wget https://raw.githubusercontent.com/FishyStix12/ConfigFisherDebian/main/Darks
 cd ~
 #Tox setup
 #Grabs the file from the download link
-wget "https://github.com/Jfreegman/toxic/releases/download/v0.15.1/toxic-minimal-static-musl_linux_x86-64.tar.xz" -O "toxcore.tar.xz"
+wget -v "https://github.com/Jfreegman/toxic/releases/download/v0.15.1/toxic-minimal-static-musl_linux_x86-64.tar.xz" -O "toxcore.tar.xz"
 #Extracts all files from archive tar
 tar -xf toxcore.tar.xz
 # Gives all scripts in the toxcore directory execute permissions
